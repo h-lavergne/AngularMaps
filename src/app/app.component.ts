208fe54e8feef22ce876129535d4c3d022f89c51
Lavergne <hugo.lavergne@devinci.fr>
2019-09-20 16:24:25 +0200

    first commit angular maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBmaNt0Q2jI-f5gaseD7oiLc_QjbUweyjo',
+  authDomain: 'angular-maps-253512.firebaseapp.com',
+  databaseURL: 'https://angular-maps-253512.firebaseio.com/',
+  projectId: 'angular-maps-253512',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'angular-maps';
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
